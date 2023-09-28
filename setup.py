72d74e81d5bfe643ffccc36597b0665290dbdea7
CyberIgor <81021293+CyberIgor@users.noreply.github.com>
2023-09-28 10:30:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='KaommatiPara-data-analysis',
+    author="Igor",
+    author_email="ihor.alfieiev@capgemini.com",
+    version='1.0',
+    description='A program for for combining client data with Bitcoin transactions data',
+    packages=find_packages(exclude=['tests']),
+    install_requires=requirements
+    )
