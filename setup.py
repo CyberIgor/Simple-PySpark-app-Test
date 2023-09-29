185931b55481c0b92e05aca8e174dc3483f99f7f
CyberIgor <81021293+CyberIgor@users.noreply.github.com>
2023-09-29 09:35:47 +0200

    Patch (#8)
    * Update pyspark-app-manual-trigger.yml
    
    * Update pyspark-app-manual-trigger.yml
    
    * Update pyspark-app-manual-trigger.yml
    
    * Update pyspark-app-manual-trigger.yml
    
    * Update pyspark-app-manual-trigger.yml
    
    * Add generated files
    
    * Add generated files
    
    * Update setup.py
    
    ---------
    
    Co-authored-by: github-actions <action@github.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='KaommatiPara-data-analysis',
     author="Igor",
